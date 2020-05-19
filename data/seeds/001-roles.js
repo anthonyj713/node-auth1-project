04f6dce14387e702ec3500d3d82c12cd6b97a004
Anthony Johnson <wontons713@Anthonys-MacBook-Pro.local>
2020-05-18 19:09:57 -0600

    seeds created

---
+++ data/seeds/001-roles.js
@@ -0,0 +1,13 @@
+
+exports.seed = function(knex) {
+
+  const roles = [
+    {
+      name: 'admin', //id 1
+    },
+    {
+      name: 'user', //id 2
+    },
+  ];
+      return knex('roles').insert(roles);
+};
